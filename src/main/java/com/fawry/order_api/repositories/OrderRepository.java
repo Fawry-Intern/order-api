bb81b8d42e61b7045283b3be8b90a4292dd45c05
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2025-04-03 02:53:14 +0200

    Delete src/main/java/com/fawry/order_api/repositories directory

--- src/main/java/com/fawry/order_api/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fawry.order_api.repositories;
-
-import com.fawry.order_api.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
